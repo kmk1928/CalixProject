e5c6416ec58eca4d462ab7c5a466ae66d8bd370f
JAeUNg <lsam3333333@gmail.com>
2023-09-23 18:33:03 +0900

    merge전에 씬 변경 신경안써도됨
    씬만 이것저것 움직임 적 공격 스크립트만 비활성화 해둠

--- Assets/Scripts/EnemyHitted.cs
+++ Assets/Scripts/EnemyHitted.cs
@@ -7,12 +7,12 @@ public class EnemyHitted : MonoBehaviour
     public int maxHP = 100;           //최대체력
     public int curHP = 100;           //현재체력
 
-    Rigidbody rigid;            //피격확인을 위함
+    //Rigidbody rigid;            //피격확인을 위함
     BoxCollider boxCollider;    //피격확인을 위함
     Material mat;               //피격 시 색상변경 확인
 
     private void Awake() {
-        rigid = GetComponent<Rigidbody>();              //피격확인을 위함
+        //rigid = GetComponent<Rigidbody>();              //피격확인을 위함
         boxCollider = GetComponent<BoxCollider>();      //피격확인을 위함\
         mat = GetComponentInChildren<MeshRenderer>().material;    //피격 시 색상변경 확인
     }
