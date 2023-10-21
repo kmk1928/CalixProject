cd7c67ee7f86611a84f18d16a68f21a19484c0b6
JAeUNg <lsam3333333@gmail.com>
2023-10-21 16:05:10 +0900

    Attack함수 파라미터 추가
    조건에 따라서 어떤 스킬배열을 사용할 건지 선택가능

--- Assets/Scripts/Player/PlayerAttack/PlayerAttacker.cs
+++ Assets/Scripts/Player/PlayerAttack/PlayerAttacker.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerAttacker : MonoBehaviour {
     ///public List<SOAttackPattern> attackPatterns_NA;
     public SOAttackPattern[] attackPatterns_NA;
-
+    public SOAttackPattern[] attackPatterns_PA;
     private float lastComboEnd;
     private float lastClickTime;
     private float lastAttackTime;
     private int comboCount; // 현재 콤보 횟수
     private int currentAttackIndex = 0;
     private bool canCombo = false; // 콤보 가능 여부
-    int maxIndex;
+    private int maxIndex;
+
+    public bool skill1Equipped = true;
 
     Animator anim;
     Transform playerTransform;
@@ -22,27 +24,28 @@ public class PlayerAttacker : MonoBehaviour {
         anim = GetComponent<Animator>();
         playerTransform = GetComponent<Transform>();
         playerController = GetComponent<PlayerController>();
-        maxIndex = attackPatterns_NA.Length - 1;
     }
     private void Update() {
         if (Input.GetButtonDown("Fire1")) {
-            Attack();
+            if (skill1Equipped) {
+                Attack(attackPatterns_NA);
+            }
         }
         ExitAttack();
     }
 
-    private void Attack() { //현재시간 - 마지막 입력 시간이 쿨다운(보통 0.4f~0.2f)보다 클때 && 현재 인덱스가 최대 인덱스보다 작거나 같을 때  
+    private void Attack(SOAttackPattern[] attackPatterns) { //현재시간 - 마지막 입력 시간이 쿨다운(보통 0.4f~0.2f)보다 클때 && 현재 인덱스가 최대 인덱스보다 작거나 같을 때  
         if (Time.time - lastComboEnd > 0.1f && currentAttackIndex <= maxIndex) {
-            // 공격 애니메이션 재생 및 데미지 적용 로직
-            Debug.Log("1111");
-            playerController.LockPlayerInput_ForAnimRootMotion();
+            // 공격 애니메이션 재생 로직
+            maxIndex = attackPatterns.Length - 1;
+            playerController.LockPlayerInput_ForAnimRootMotion();   //플레이어 이동제한
             CancelInvoke("EndCombo");
-            if(Time.time - lastClickTime >= attackPatterns_NA[currentAttackIndex].cooldown) {
+            if(Time.time - lastClickTime >= attackPatterns[currentAttackIndex].cooldown) {
                 Debug.Log("attackgogo");
-                anim.runtimeAnimatorController = attackPatterns_NA[currentAttackIndex].animatorOV;
+                anim.runtimeAnimatorController = attackPatterns[currentAttackIndex].animatorOV;
                 anim.Play("NormalAttack", 0, 0);
                 #region 파티클 효과 생성
-                SOAttackPattern currentAttack = attackPatterns_NA[currentAttackIndex];
+                SOAttackPattern currentAttack = attackPatterns[currentAttackIndex];
                 Vector3 position = playerTransform.position + currentAttack.particlePosition;
                 Quaternion rotation = playerTransform.rotation * Quaternion.Euler(currentAttack.particleRotation);
                 Vector3 scale = currentAttack.particleScale;
