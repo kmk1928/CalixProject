dfc4641bd68d77beb7722a06b226ebedcf5cd479
jun123188 <jun123188@naver.com>
2023-10-23 21:06:17 +0900

    툴팁창 구현완료
    완

--- Assets/02.Scripts/UI Script/Slot.cs
+++ Assets/02.Scripts/UI Script/Slot.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
     public Item item; // 획득한 아이템
     public Image itemImage; // 아이템의 이미지
     public int itemCount; // 획득한 아이템의 개수
+
+    [SerializeField]
+    private SlotToolTip theSlotToolTip; // 슬롯 툴팁 호출
+
+    void Start()
+    {
+        theSlotToolTip = FindObjectOfType<SlotToolTip>();
+    }
+
+    public void ShowToolTip(Item _item)
+    {
+        theSlotToolTip.ShowToolTip(_item);
+    }
+
+    public void HideToolTip()
+    {
+        theSlotToolTip.HideToolTip();
+    }
 
     private void SetColor(float _alpha)
     {
@@ -16,9 +34,8 @@ public class Slot : MonoBehaviour
         color.a = _alpha;
         itemImage.color = color;
     }
-    
 
-        //아이템 획득
+    // 아이템 획득
     public void AddItem(Item _item, int _count = 1)
     {
         item = _item;
@@ -28,12 +45,12 @@ public class Slot : MonoBehaviour
         SetColor(1);
     }
 
-    //아이템 개수 조정
+    // 아이템 개수 조정
     public void SetSlotCount(int _count)
     {
         itemCount += _count;
-        
-        if(itemCount <= 0)
+
+        if (itemCount <= 0)
             ClearSlot();
     }
 
@@ -46,4 +63,14 @@ public class Slot : MonoBehaviour
         SetColor(0);
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (item != null)
+            ShowToolTip(item);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HideToolTip();
+    }
 }
