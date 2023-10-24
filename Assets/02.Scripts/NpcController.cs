c9cf6e6ba16d492618365c6f0871447d85fdfa66
Woodaeil <rkawk927@gmail.com>
2023-10-25 06:45:59 +0900

    상점에 플레이어추가 상인 애니메이션 추가
    카메라 관련 오류 뜨는데 이거 해결 좀

--- Assets/02.Scripts/NpcController.cs
+++ Assets/02.Scripts/NpcController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class NpcController : MonoBehaviour
 {
-    Animator anim;//animation variable
+    public Transform player; // 플레이어 게임 오브젝트
+    Animator anim; // animation 변수
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();//animation
+        anim = GetComponent<Animator>(); // animation
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (6 < distanceToPlayer && distanceToPlayer <= 8) // 플레이어 발견 시
+            {
+                anim.SetBool("Greeting", true); // SetBool 메서드로 수정
+            }
+            else
+            {
+                anim.SetBool("Greeting", false); // else 블록에서 false로 설정하여 애니메이션을 끄세요.
+            }
+        }
     }
 }
