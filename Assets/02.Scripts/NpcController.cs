8f85ae271916d9138c4f8d1bfda3fc095e26bd2a
Woodaeil <rkawk927@gmail.com>
2023-10-25 03:19:07 +0900

    npc애니메이션 추가

---
+++ Assets/02.Scripts/NpcController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{
+    Animator anim;//animation variable
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();//animation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
