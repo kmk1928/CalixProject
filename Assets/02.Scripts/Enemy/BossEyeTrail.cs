0c79f9d11c6c08b459c7553205876f9f1553c0a5
JAeUNg <lsam3333333@gmail.com>
2023-10-27 21:02:12 +0900

    보스 공격 범위 추가

--- Assets/02.Scripts/Enemy/BossEyeTrail.cs
+++ Assets/02.Scripts/Enemy/BossEyeTrail.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class BossEyeTrail : MonoBehaviour
 {
     TrailRenderer trailRenderer;
-    public float trailDuration = 0.5f; // 트레일 지속 시간
-    public float trailWidth = 0.1f; // 트레일 너비
+    //public float trailDuration = 0.5f; // 트레일 지속 시간
+    //public float trailWidth = 0.01f; // 트레일 너비
     public ParticleSystem particle_red;
     public ParticleSystem particle_distortion;
     public Light eyeLight;
@@ -14,10 +14,10 @@ public class BossEyeTrail : MonoBehaviour
     {
         eyeLight = GetComponentInChildren<Light>();
         trailRenderer = GetComponentInChildren<TrailRenderer>();
-        // 트레일 렌더러 설정
-        trailRenderer.time = trailDuration;
-        trailRenderer.startWidth = trailWidth;
-        trailRenderer.endWidth = 0.0f; // 끝 부분이 얇게 설정하여 페이드 아웃 효과를 만듭니다.
+        //// 트레일 렌더러 설정
+        //trailRenderer.time = trailDuration;
+        //trailRenderer.startWidth = trailWidth;
+        //trailRenderer.endWidth = 0.0f; // 끝 부분이 얇게 설정하여 페이드 아웃 효과를 만듭니다.
 
     }
 
