f33f4d564acd417bf2451faef89ef9a73ee43621
Woodaeil <rkawk927@gmail.com>
2023-10-31 07:14:05 +0900

    플레이어에 모션 사운드 추가

---
+++ Assets/02.Scripts/Player/PlayerSound.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+   // Animator 컴포넌트
+   Animator anim;
+   
+   // 발소리 오디오 클릭
+   public AudioClip footstep;
+   public AudioClip jump;
+   public AudioClip attack;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      // Animator 컴포넌트 가져오기
+      anim = GetComponent<Animator>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+   }
+
+   // 애니메이션 이벤트
+   void FootStep()
+   {
+      // 이벤트가 발생하면 사운드 재생
+      AudioSource.PlayClipAtPoint(footstep, Camera.main.transform.position);
+   }
+   void Jump()
+   {
+      // 이벤트가 발생하면 사운드 재생
+      AudioSource.PlayClipAtPoint(jump, Camera.main.transform.position);
+   }
+   void Attack()
+   {
+      // 이벤트가 발생하면 사운드 재생
+      AudioSource.PlayClipAtPoint(attack, Camera.main.transform.position);
+   }
+}
