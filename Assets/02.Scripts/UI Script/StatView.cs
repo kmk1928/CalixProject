4e82b8197a13608b031239f734312856a7457987
jun123188 <jun123188@naver.com>
2023-11-04 06:18:42 +0900

    능력치창 간단구현
    슬롯뷰 스크립트 업데이트함수에
    플레이어스탯 인자로 받고
    각 능력치오브젝트에 수치덮어씌우기
    구현요망

---
+++ Assets/02.Scripts/UI Script/StatView.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatView : MonoBehaviour
+{
+    //필요한 컴포넌트들
+    [SerializeField]
+    private GameObject Stats;
+
+    [SerializeField]
+    private TextMeshProUGUI AD_Stat;
+
+    [SerializeField]
+    private TextMeshProUGUI DF_Stat;
+
+    [SerializeField]
+    private TextMeshProUGUI HP_stat;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
