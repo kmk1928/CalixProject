535a349cc768cb65d624f857fe42583eb617e940
JAeUNg <lsam3333333@gmail.com>
2023-11-04 18:34:42 +0900

    flymech스킬 트레일 추가

---
+++ Assets/02.Scripts/Weapon/SkillTrail.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTrail : MonoBehaviour
+{
+    public MeleeWeaponTrail flymech_Trail;
+
+    public void FlyMech_Trail()
+    {
+        flymech_Trail._emitTime = 0.2f;
+        flymech_Trail.Emit = true;
+    }
+
+}
