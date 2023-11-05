9b1f7a609ec310e0d73c487d23c9de740f078b1c
jun123188 <jun123188@naver.com>
2023-11-06 03:57:23 +0900

    보너스 스텟 표기
    보너스 스텟 표기

--- Assets/02.Scripts/UI Script/StatView.cs
+++ Assets/02.Scripts/UI Script/StatView.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class StatView : MonoBehaviour
 {
     [SerializeField]
-    private PlayerStats playerStats; // Inspector에서 PlayerStats 컴포넌트 할당
+    private PlayerStats playerStats;
 
     [SerializeField]
     private TextMeshProUGUI AD_Stat;
@@ -17,6 +17,24 @@ public class StatView : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI HP_stat;
 
+    [SerializeField]
+    private TextMeshProUGUI AD_StatBonus1;
+
+    [SerializeField]
+    private TextMeshProUGUI DF_StatBonus1;
+
+    [SerializeField]
+    private TextMeshProUGUI HP_StatBonus1;
+
+    [SerializeField]
+    private TextMeshProUGUI AD_StatBonus2;
+
+    [SerializeField]
+    private TextMeshProUGUI DF_StatBonus2;
+
+    [SerializeField]
+    private TextMeshProUGUI HP_StatBonus2;
+
     void Update()
     {
         if (playerStats != null)
@@ -26,17 +44,81 @@ public class StatView : MonoBehaviour
             float attackDamage = playerStats.attackDamage;
             float defense = playerStats.defense;
 
+            int redCount = playerStats.redCount;
+            int yellowCount = playerStats.yellowCount;
+            int blueCount = playerStats.blueCount;
+
             // 텍스트 업데이트
             AD_Stat.text = attackDamage.ToString();
             DF_Stat.text = defense.ToString();
             HP_stat.text = maxHealth.ToString();
+
+            AD_statBonus1();
+            AD_statBonus2();
+            DF_statBonus1();
+            DF_statBonus2();
+            HP_statBonus1();
+            HP_statBonus2();
         }
         else
         {
             Debug.LogWarning("PlayerStats 컴포넌트를 찾을 수 없습니다.");
         }
     }
+
+    public void AD_statBonus1()
+    {
+        if (playerStats.redCount >= 3)
+            AD_StatBonus1.enabled = true;
+        else
+            AD_StatBonus1.enabled = false;
+    }
+
+    public void AD_statBonus2()
+    {
+        if (playerStats.redCount >= 6)
+            AD_StatBonus2.enabled = true;
+        else
+            AD_StatBonus2.enabled = false;
+    }
+
+    public void DF_statBonus1()
+    {
+        if (playerStats.blueCount >= 3)
+            DF_StatBonus1.enabled = true;
+        else
+            DF_StatBonus1.enabled = false;
+    }
+
+    public void DF_statBonus2()
+    {
+        if (playerStats.blueCount >= 6)
+            DF_StatBonus2.enabled = true;
+        else
+            DF_StatBonus2.enabled = false;
+    }
+
+    public void HP_statBonus1()
+    {
+        if (playerStats.yellowCount >= 3)
+            HP_StatBonus1.enabled = true;
+        else
+            HP_StatBonus1.enabled = false;
+    }
+
+    public void HP_statBonus2()
+    {
+        if (playerStats.yellowCount >= 6)
+            HP_StatBonus2.enabled = true;
+        else
+            HP_StatBonus2.enabled = false;
+    }
 }
+
+
+
+
+
 
 
 
