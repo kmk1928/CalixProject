5186b62ce29b775b8d7297213676ca5d2ae4ea13
JAeUNg <lsam3333333@gmail.com>
2023-11-10 22:39:15 +0900

    스폰 위치 조정

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public int portalNumber;
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            player.transform.position = new Vector3(0, 0, 0); // 플레이어의 위치를 (0, 0, 0)으로 설정
+        }
+    }
 }
